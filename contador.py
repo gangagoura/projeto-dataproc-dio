a386ca3403546e727a15f498b6172eb83838c12b
GUILHERME FERNANDES NETO <40431403+gangagoura@users.noreply.github.com>
2021-08-09 13:43:43 -0300

    Update contador.py

--- contador.py
+++ contador.py
@@ -2,6 +2,6 @@ import sys
 from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
     sc = SparkContext("local","PySpark Exemplo - Desafio Dataproc")
-    words = sc.textFile("gs://{SEU_BUCKET}/livro.txt").flatMap(lambda line: line.split(" "))
+    words = sc.textFile("gs://{desafiodataproc666}/livro.txt").flatMap(lambda line: line.split(" "))
     wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
-    wordCounts.saveAsTextFile("gs://{SEU_BUCKET}/resultado")
+    wordCounts.saveAsTextFile("gs://{desafiodataproc666}/resultado")
